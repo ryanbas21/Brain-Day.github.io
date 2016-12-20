12cc014181684b6bce2988e777180198b9825a4f
Edward Guo <edward.h.guo@gmail.com>
2016-12-19 22:42:58 -0800

    documentaiton styling

--- src/documentation/doc-container.js
+++ src/documentation/doc-container.js
@@ -1,20 +1,21 @@
-import Inferno from 'inferno';
-import Component from 'inferno-component';
+import Inferno from 'inferno'
+import Component from 'inferno-component'
 import Documentation from './Documentation'
-import NavBar from '../navbar';
-import SideNav from './side-nav';
+import NavBar from '../navbar'
+import SideNav from './side-nav'
+
 const Docs = () =>
-      <div className='pagination-center-example'>
-        <NavBar />
-          <div className=''>
-            <div className='div-one-hundred'>
-              <SideNav/>
-            </div>
-          </div>
-          <div className='large-6 columns'>
-          <Documentation/>
-          </div>
-          <div className='large-3 columns'> </div>
-      </div>
+  <div>
+    
+    <NavBar />
+    <div>
+        <SideNav />
+    </div>
+
+    <div>
+      <Documentation />
+    </div>
+
+  </div>
 
 export default Docs
--- src/documentation/side-nav.js
+++ src/documentation/side-nav.js
@@ -1,18 +1,25 @@
-import Inferno from 'inferno';
+import Inferno from 'inferno'
 
 const SideNav = () =>
-<ul className="side-nav no-bullet" role='navigation' title="Link List">
-        <li role='menuitem'className='divider'></li>
-        <li role='menuitem'className='center-doc title'><a href='#api-reference'>API REFERENCE</a></li>
-        <li role='menuitem'className='center-doc item'><a href="#combine-reducer">CombineReducers</a></li>
-        <li role='menuitem' className='center-doc item'><a href="#get-state">Get State</a></li>
-        <li role='menuitem'className='center-doc item'><a href="#dispatch">Dispatch</a></li>
-        <li role='menuitem'className='center-doc item'><a href="#subscribe">Subscribe</a></li>
-        <li role='menuitem'className='divider'></li>
-        <li role='menuitem'className='center-doc title'><a href='#actions'>Actions</a></li>
-        <li role='menuitem'className='center-doc item'><a href='#lock-specific-key-paths'>Lock Key Paths</a></li>
-        <li role='menuitem'className='center-doc item'><a href="#locked-state">Lock State</a></li>
-
-</ul>
+  <nav>
+    <ul className="sidenav">
+      <li>
+        <a href='#api-reference'>API REFERENCE</a></li>
+      <li>
+        <a href="#combine-reducer">CombineReducers</a></li>
+      <li>
+        <a href="#get-state">Get State</a></li>
+      <li>
+        <a href="#dispatch">Dispatch</a></li>
+      <li>
+        <a href="#subscribe">Subscribe</a></li>
+      <li>
+        <a href='#actions'>Actions</a></li>
+      <li>
+        <a href='#lock-specific-key-paths'>Lock Key Paths</a></li>
+      <li>
+        <a href="#locked-state">Lock State</a></li>
+    </ul>
+  </nav>
 
 export default SideNav;
